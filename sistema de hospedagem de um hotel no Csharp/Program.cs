fdf03db05b8e7b8c43730d6abddc63e0075df83e
fq38 <fq38.live.com>
2022-09-23 22:08:45 -0300

    Adicionar arquivos de projeto.

---
+++ sistema de hospedagem de um hotel no Csharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+using 
+
+    Console.outputEcoding = Encofing.Utf8;
+
+List<pessoa> hospedes = new List<Pessoa>();
+
+Pessoa p1 = new Pessoa(nome: "hospede 1");
+
+hospedes.add(p1);
+
+Suite suite = new Suite(tipoSuite: "premiun",capacidade: 3, valordia)
+
+    Reserva reserva = new Reserva(diasReservados: 10);
+
+global::System.Console.WriteLine("Hospedes: {reserva.obterqunatidade()}");
